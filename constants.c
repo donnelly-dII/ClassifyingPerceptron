1272185d4242bff3a92447f8aec86081cb8cf424
donnelly-dII <donnellyd215@gmail.com>
2018-05-29 10:28:40 -0500

    add constants file

---
+++ constants.c
@@ -0,0 +1,13 @@
+//
+//  constants.c
+//  ClassifyingPerceptron
+//
+//  Created by David Donnelly II on 5/29/18.
+//  Copyright © 2018 DavidDonnellyII. All rights reserved.
+//
+#define PI 3.14159
+#define TRAINING_SIZE 1000
+#define TEST_SIZE 100
+#define NUM_CLASSES 9
+#define NUM_FEATURES 3
+#define DATA_DIM 2
